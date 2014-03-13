512f2a6261a61c17f73f8c5990978a4669aec32b
Warren Downs <vwdowns@iglooware.com>
2014-03-12 16:45:52 -0800

    Added sudo to install

--- doinstall.sh
+++ doinstall.sh
@@ -7,15 +7,15 @@ if [ "$1" = "--install" ]; then
   cd .. && git clone https://github.com/downwa/tcprelay && cd tcprelay/ && ./configure && sudo make install && cd ../iglooportal/
 
 	# Install or update needed software
-	apt-get install arptables bridge-utils conntrack dnsutils ebtables git nmap realpath dnsmasq isc-dhcp-server
+	sudo apt-get install arptables bridge-utils conntrack dnsutils ebtables git nmap realpath dnsmasq isc-dhcp-server
 
 	# Fix permissions before copy
   sudo chown root etc/sudoers.d/
   sudo chown root ./etc/sudoers.d/www-data-allow
 fi
 
-cp -av etc/ usr/ home/ /
-/etc/init.d/iglooportal restart
-ifup -a
-/etc/init.d/isc-dhcp-server start
+sudo cp -av etc/ usr/ home/ /
+sudo /etc/init.d/iglooportal restart
+sudo ifup -a
+sudo /etc/init.d/isc-dhcp-server start
 
