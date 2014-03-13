7029240f6ba03ccacd711a8e43a10787572afb1f
Warren Downs <wdowns@choggiung.com>
2014-03-13 08:45:50 -0800

    Fixes to doinstall script

--- doinstall.sh
+++ doinstall.sh
@@ -2,17 +2,21 @@
 # Install script
 
 if [ "$1" = "--install" ]; then
-  # git clone https://github.com/downwa/iglooportal # NOTE: Presumably already done if you are here
+	# git clone https://github.com/downwa/iglooportal # NOTE: Presumably already done if you are here
 
-  # Install tcprelay from repository
-  cd .. && git clone https://github.com/downwa/tcprelay && cd tcprelay/ && ./configure && sudo make install && cd ../iglooportal/
+	# Install tcprelay from repository
+	cd ..
+	git clone https://github.com/downwa/tcprelay && cd tcprelay/ && ./configure && sudo make install && cd ..
+	cd iglooportal/
+
+	pwd
 
 	# Install or update needed software
-	sudo apt-get install arptables bridge-utils conntrack dnsutils ebtables git nmap realpath dnsmasq isc-dhcp-server
+	sudo apt-get --assume-yes install arptables bridge-utils conntrack dnsutils ebtables git nmap realpath dnsmasq isc-dhcp-server
 
 	# Fix permissions before copy
-  sudo chown root etc/sudoers.d/
-  sudo chown root ./etc/sudoers.d/www-data-allow
+	sudo chown root etc/sudoers.d/
+	sudo chown root ./etc/sudoers.d/www-data-allow
 fi
 
 sudo cp -av etc/ usr/ home/ /
